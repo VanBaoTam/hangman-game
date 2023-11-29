a8c25c881fc13d0b659d59316cbab6603c7966c2
VoVietMy <vovietmy12@gmail.com>
2023-11-29 11:13:51 +0700

    add:display_man

--- main.cpp
+++ main.cpp
@@ -1,8 +1,83 @@
 #include <iostream>
 using namespace std;
+void display_man(int misses){
+	if( misses ==0){
+	cout<<" +---+\n";
+	cout<<" |   |\n";
+	cout<<"     |\n";
+	cout<<"     |\n";
+	cout<<"     |\n";
+	cout<<"     |\n";
+	cout<<"======== \n";
+	}
+	else if(misses ==1)
+	{
+	cout<<" +---+\n";
+	cout<<" |   |\n";
+	cout<<" O   |\n";
+	cout<<"     |\n";
+	cout<<"     |\n";
+	cout<<"     |\n";
+	cout<<"======== \n";	
+	}
+	else if(misses ==2)
+	{
+	cout<<" +---+\n";
+	cout<<" |   |\n";
+	cout<<" O   |\n";
+	cout<<" |   |\n";
+	cout<<"     |\n";
+	cout<<"     |\n";
+	cout<<"======== \n";	
+	}
+	else if(misses ==3)
+	{
+	cout<<" +---+\n";
+	cout<<" |   |\n";
+	cout<<" O   |\n";
+	cout<<" |   |\n";
+	cout<<"/    |\n";
+	cout<<"     |\n";
+	cout<<"======== \n";	
+	}
+	else if(misses ==4)
+	{
+	cout<<" +---+\n";
+	cout<<" |   |\n";
+	cout<<" O   |\n";
+	cout<<"/|\\  |\n";
+	cout<<"     |\n";
+	cout<<"     |\n";
+	cout<<"======== \n";	
+	}
+	else if(misses ==5)
+	{
+	cout<<" +---+\n";
+	cout<<" |   |\n";
+	cout<<" O   |\n";
+	cout<<"/|\\  |\n";
+	cout<<"/    |\n";
+	cout<<"     |\n";
+	cout<<"======== \n";	
+	}
+else if(misses ==6)
+	{
+	cout<<" +---+\n";
+	cout<<" |   |\n";
+	cout<<" O   |\n";
+	cout<<"/|\\  |\n";
+	cout<<"/ \\  |\n";
+	cout<<"     |\n";
+	cout<<"======== \n";	
+	}
+}
+
+
 int main()
 {
-      cout << "Hello world";
-
+	string codeword ="codingleverly";
+	string answer="----------";
+	int misses = 5;
+		display_man(misses);
       return 0;
 }
