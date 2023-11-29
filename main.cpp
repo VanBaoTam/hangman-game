f539e85d3cab8e6e5dca372cf83331fe23047362
VanBaoTam <vanbaotam187@gmail.com>
2023-11-29 10:57:46 +0700

    add:database | get_random_word

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
+
+struct Word
+{
+      string word;
+      int n;
+};
+
+void initializeWordArray(Word wordArray[], int size)
+{
+      string defaultWords[] = {"information", "technology", "administrative", "business", "coding", "office", "learning", "opensource", "management", "abbreviation"};
+
+      for (int i = 0; i < size; ++i)
+      {
+            wordArray[i].word = defaultWords[i];
+            wordArray[i].n = wordArray[i].word.length();
+      }
+}
+
+Word getRandomWord(const Word wordArray[], int size)
+{
+      srand(time(NULL));
+
+      int randomIndex = rand() % size;
+      return wordArray[randomIndex];
+}
+
 int main()
 {
-      cout << "Hello world";
+
+      const int arraySize = 10;
+      Word myWordArray[arraySize];
+      initializeWordArray(myWordArray, arraySize);
+      Word randomWord = getRandomWord(myWordArray, arraySize);
+      std::cout << "\nRandom Word: " << randomWord.word << "\nLength: " << randomWord.n;
 
       return 0;
 }
