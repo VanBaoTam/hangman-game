633bc366d4c1e94a2f779cac5a873f25674f6f47
VanBaoTam <vanbaotam187@gmail.com>
2023-11-29 11:21:41 +0700

    add: display_status

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+void display_status(vector<char> incorrect, string answer)
+{
+      cout << "\nIncorrect guesses: \n";
+      for (int i = 0; i < incorrect.size(); i++)
+      {
+            cout << incorrect[i] << "  ";
+      }
+      cout << "\nCode word:\n";
+      for (int i = 0; i < answer.length(); i++)
+      {
+            cout << answer[i] << "  ";
+      }
+}
 int main()
 {
-      cout << "Hello world";
-
       return 0;
 }
