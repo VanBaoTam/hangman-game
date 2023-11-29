0968e06a5350b622d72789c2e88bcd64126094b0
VoVietMy <vovietmy12@gmail.com>
2023-11-29 11:28:34 +0700

    add:notification when game end

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
+void end_game(string answer, string codeword)
+{
+	if(answer ==codeword)
+	{
+		cout<<"Hooray! You saved the person from being hanged and earned a medal of honor!\n";
+		cout<<"Congratulations !"<<endl;
+	}
+	else
+	{
+		cout<<"Oh shit! Your friend is hanged!\n";
+	}
+}
 int main()
 {
-      cout << "Hello world";
-
+      end_game("test","test");
+	  end_game("test","notest");
       return 0;
 }
