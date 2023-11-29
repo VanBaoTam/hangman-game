9609817f8639cf3a3e50a15a417a34e444553a5f
VanBaoTam <vanbaotam187@gmail.com>
2023-11-29 11:04:47 +0700

    hotfix

--- main.cpp
+++ main.cpp
@@ -23,7 +23,6 @@ void initializeWordArray(Word wordArray[], int size)
 
 Word getRandomWord(const Word wordArray[], int size)
 {
-      srand(time(NULL));
 
       int randomIndex = rand() % size;
       return wordArray[randomIndex];
@@ -31,12 +30,11 @@ Word getRandomWord(const Word wordArray[], int size)
 
 int main()
 {
-
+      srand(time(NULL));
       const int arraySize = 10;
       Word myWordArray[arraySize];
       initializeWordArray(myWordArray, arraySize);
       Word randomWord = getRandomWord(myWordArray, arraySize);
       std::cout << "\nRandom Word: " << randomWord.word << "\nLength: " << randomWord.n;
-
       return 0;
 }
