dbd8818cf1f9e4afcb32b08fab92b0715c8192aa
VoVietMy <vovietmy12@gmail.com>
2023-11-29 10:20:22 +0700

    add:game_title

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
+
+void game_title(){
+cout << "======================\n";
+cout << "Game Title\n";
+cout << "======================\n";	
+}
 int main()
-{
+{	game_title();
       cout << "Hello world";
 
       return 0;
